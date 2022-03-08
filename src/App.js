375487b3863a2cbf1c134a834834c86e03309622
Marc Cavazos <marcavazos19@icloud.com>
2022-03-08 10:54:53 -0600

    router basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
 import NavBar from "./NavBar";
 import RestaurantPage from "./RestaurantPage";
+import Home from './Home';
+import MyReservations from './MyReservations';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <RestaurantPage />
+      <Switch>
+        <Route>
+          <RestaurantPage path='/restaurants'/>
+        </Route>
+        <Route>
+          <MyReservations path='reservations' />
+        </Route>
+        <Route>
+          <Home path='/'/>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <>
-    <div> About </div>
-    <div> Restaurants </div>
-    <div> My Reservations </div>
-    </>
+    <header>
+      <NavLink to='/'>home</NavLink>
+      <NavLink to='/restaurants'>restaurants</NavLink>
+      <NavLink to='/reservations'>reservations</NavLink>
+    </header>
   )
 }
 
